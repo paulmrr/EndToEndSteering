ce1a673f86f8ff2769c9fc707f3c6c7c7998176d
Chris Varanese <cvaranes@calpoly.edu>
2018-06-05 22:45:43 -0700

    model

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, MaxPooling2D, Conv2D
+from keras.layers import Dense, MaxPooling2D, Conv2D, Flatten, Dropout
 import numpy as np
 
-img_size = (260, 640, 3)
+img_size = (210, 640, 3)
 
 def build_model():
     
@@ -11,13 +11,13 @@ def build_model():
     model.add(Conv2D(32, (5, 5), activation='relu',
               input_shape=(img_size)))
     model.add(Conv2D(32, (5, 5), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(MaxPooling2D((2, 2)))
 
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2, 2)))
 
-    model.add(layers.Flatten())
+    model.add(Flatten())
     model.add(Dense(128, activation='sigmoid'))
     model.add(Dropout(.5))
 
@@ -27,12 +27,29 @@ def build_model():
         optimizer='adam',
         loss='mse',
     )
+    print(model.summary())
 
     return model
 
 def train_model(model):
-    X_train, y_train, X_test, y_test = load_tesla_data()
+    X_train = np.load('data/train_data.npy', mmap_mode='r')
+    y_train = np.load('data/train_labels.npy', mmap_mode='r')
+    X_test = np.load('data/test_data.npy', mmap_mode='r')
+    y_test = np.load('data/test_labels.npy', mmap_mode='r')
 
-    model.fit(X_train, y_train, batch_size=32, epochs=5,
-              validation_data=(X_test, y_test))
+    print(X_train)
+    print(y_train)
 
+    print("starting...")
+    # model.fit(X_train, y_train, batch_size=32, epochs=5,
+    #           validation_data=(X_test, y_test))
+    model.fit(X_train, y_train, batch_size=32, epochs=5)
+    print("ending...")
+
+def main():
+    model = build_model()
+    train_model(model)
+
+
+if __name__ == "__main__":
+    main()
